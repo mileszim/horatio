0a85a3ef204a2d7e03e837db1eb81d8a969cf4d2
Miles Zimmerman <miles@zserver.org>
2013-10-23 15:52:14 -0700

    adding some stuff

--- scripts/horatio.js
+++ scripts/horatio.js
@@ -1,4 +1,7 @@
-
-var Horatio = function() {
-  
-}
+/**
+ * Horatio
+ * A Javascript compiler for the Shakespeare Programming Language
+ *
+ * @author Miles Zimmerman
+ */
+var Horatio = Horatio || {};
---
+++ scripts/lib/wordlists.js
@@ -0,0 +1,25 @@
+/**
+ * Horatio Wordlists
+ * Holds syntax for parsing.
+ */
+Horatio.Wordlist = {
+  
+  /** Articles */
+  articles: [
+    'a',
+    'an',
+    'the'
+  ],
+  
+  
+  /** Be */
+  be: [
+    'am',
+    'are',
+    'art',
+    'be',
+    'is'
+  ]
+  
+  
+};
