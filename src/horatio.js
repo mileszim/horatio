13b736ad6949883dcbbbe7ae176f75a4d52a31d5
Miles Zimmerman <mzimmerman@invoca.com>
2015-07-21 01:45:18 -0700

    Updating Horatio

--- src/horatio.js
+++ src/horatio.js
@@ -1,22 +1,13 @@
+import Compiler from 'compiler';
+
 /**
  * Horatio
  * A Javascript compiler for the Shakespeare Programming Language
  *
  * @author Miles Zimmerman
  */
-
-/**
- * Horatio Namespace
- * @namespace
- */
-var Horatio = Horatio || {
-  
-  // Is this running on nodejs?
-  // Borrowed with love from moment.js
-  hasModule: (typeof module !== 'undefined' && module.exports && typeof require !== 'undefined')
-  
-};
-
-
-/** Expose */
-if (Horatio.hasModule) module.exports = Horatio;
+export default class Horatio extends Compiler {
+  constructor(io) {
+    super(io);
+  }
+}
